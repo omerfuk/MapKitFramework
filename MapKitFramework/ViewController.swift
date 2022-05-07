a7bfd7ea32b89ee83bda60831aba905c14d22b3e
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-07 13:47:14 +0300

    .

--- MapKitFramework/ViewController.swift
+++ MapKitFramework/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
-
+import MapKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //41, 28
+        
+        let konum = CLLocationCoordinate2D(latitude: 41, longitude: 28)
+        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+        
+        let bolge = MKCoordinateRegion(center: konum, span: span)
+        
+        mapView.setRegion(bolge, animated: true)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = konum
+        pin.title = "Taksim"
+        pin.subtitle = "Alt Başlık"
+        
+        mapView.addAnnotation(pin)
     }
 
 
